c735c644e1a8aa9a571410bad0d1313a13596af8
Jhony Dalcin <jhonydalcin@gmail.com>
2026-01-30 10:08:07 -0300

    project created

---
+++ src/main/java/com/github/jhonydalcin/cellsmanagement/CellsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.jhonydalcin.cellsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CellsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jhonydalcin/cellsmanagement/CellsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jhonydalcin.cellsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
